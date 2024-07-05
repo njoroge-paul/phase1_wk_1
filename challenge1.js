a28266a3622cf3435ac044ffd82750a3fddb8e91
Duncan <njorogedun870@gmail.com>
2024-07-05 12:43:41 +0300

    completed challenge 1

---
+++ challenge1.js
@@ -0,0 +1,18 @@
+//student grade generator
+function studentGradeGenerator(score)  {
+  if (score > 79) {
+    return 'A';
+  } else if (score >= 60 && score <= 79) {
+    return 'B';
+  } else if (score >= 49 && score <= 59) {
+    return 'C';
+  } else if (score >= 40 && score <= 49) {
+    return 'D';
+  } else {
+    return 'F';
+  }
+  
+ 
+ 
+};
+console.log(studentGradeGenerator(90)); 
